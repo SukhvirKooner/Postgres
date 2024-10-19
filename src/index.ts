a31950c772c3612afaa1bae84f0bfb774f84bbe5
SukhvirKooner <sukhvirkooner05@gmail.com>
2024-10-19 19:02:51 +0530

    protection from sql injection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 // write a function to create a users table in your database.
 import { Client } from 'pg'
  
-const client = new Client({
-  connectionString: "postgresql://postgres1_owner:Aij09yLSKCkZ@ep-raspy-surf-a5m49wte.us-east-2.aws.neon.tech/postgres1?sslmode=require"
-})
 
-async function createUsersTable() {
-    await client.connect()
 
+// async function createUsersTable() {
+//     await client.connect()
+//}
     // const result = await client.query(`
     //     CREATE TABLE users (
     //         id SERIAL PRIMARY KEY,
@@ -17,11 +15,32 @@ async function createUsersTable() {
     //         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     //     );
     // `)
-    const result = await client.query(`
-        INSERT INTO users (username, email, password) VALUES ('sukh', 'sukhvir@example.com', 'sukhvir193');
-        `)
     
-    console.log(result) 
-}
+    // const result = await client.query(`
+    //     INSERT INTO users (username, email, password) VALUES ('sukh', 'sukhvir@example.com', 'sukhvir193');
+    // `)
+    
+    // console.log(result) 
 
-createUsersTable();
+    async function insertData(username: string, email: string, password: string) {
+        const client = new Client({
+            connectionString: "postgresql://postgres1_owner:Aij09yLSKCkZ@ep-raspy-surf-a5m49wte.us-east-2.aws.neon.tech/postgres1?sslmode=require"
+          })
+      
+        try {
+          await client.connect(); // Ensure client connection is established
+          // Use parameterized query to prevent SQL injection
+          const insertQuery = "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)";
+          const values = [username, email, password];
+          const res = await client.query(insertQuery, values);
+          console.log('Insertion success:', res); // Output insertion result
+        } catch (err) {
+          console.error('Error during the insertion:', err);
+        } finally {
+          await client.end(); // Close the client connection
+        }
+      }
+
+      insertData('username5', 'user5@example.com', 'user_password').catch(console.error);
+
+
